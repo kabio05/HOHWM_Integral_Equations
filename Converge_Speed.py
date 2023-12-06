4d3ec11b8533b59774756140b8a0f11405b4f49c
Yi Zong <yi.zong20@imperial.ac.uk>
2023-12-06 16:18:23 +0000

    WHY???

--- Converge_Speed.py
+++ Converge_Speed.py
@@ -10,37 +10,78 @@ err_local = np.zeros(len(col_size))
 err_global = np.zeros(len(col_size))
 s_text = ["1st", "2nd"]
 
-for test_type in ["Fredholm", "Volterra"]:
+for test_type in ["Fredholm"]:
     if test_type == "Fredholm":
         f = lambda x: np.exp(x) + np.exp(-x)
         K = lambda x, t: -np.exp(-(x + t))
         u_true = lambda x: np.exp(x)
         test = HOHWM.IntegralEquation(linear=True, type="Fredholm", f=f, K=K)
 
+        # f = lambda x: 5/6 * x - 1/9
+        # K = lambda x, t: 1/3 * (x + t)
+        # u_true = lambda x: x
+        # test = HOHWM.IntegralEquation(linear=True, type="Fredholm", f=f, K=K)
+        
+        # f = lambda x: 2/3 * x 
+        # K = lambda x, t: x * t
+        # u_true = lambda x: x
+        # test = HOHWM.IntegralEquation(linear=True, type="Fredholm", f=f, K=K)
+        
+        # f = lambda x: 9/10 * x**2
+        # K = lambda x, t: 1/2 * (x**2 * t**2)
+        # u_true = lambda x: x**2
+        # test = HOHWM.IntegralEquation(linear=True, type="Fredholm", f=f, K=K)
+        
+        # f = lambda x: np.exp(x) - 1
+        # K = lambda x, t: t
+        # u_true = lambda x: np.exp(x)
+        # test = HOHWM.IntegralEquation(linear=True, type="Fredholm", f=f, K=K)
+        
+        f = lambda x: np.exp(3 * x) - 1/9 * (2 * np.exp(3) + 1) * x
+        K = lambda x, t: x * t
+        u_true = lambda x: np.exp(3 * x)
+        test = HOHWM.IntegralEquation(linear=True, type="Fredholm", f=f, K=K)
+        
     if test_type == "Volterra":
-        f = lambda x: 1/2 * x**2 * np.exp(-x)
-        K = lambda x, t: 1/2 * (x - t)**2 * np.exp(-x + t)
-        u_true = lambda x: 1/3 - 1/3 * np.exp(-3/2 * x) * (
-        np.cos(np.sqrt(3)/2 * x) + np.sqrt(3) * np.sin(np.sqrt(3)/2 * x))
-        test = HOHWM.IntegralEquation(linear=True, type="Volterra", f=f, K=K)
+        # f = lambda x: 1/2 * x**2 * np.exp(-x)
+        # K = lambda x, t: 1/2 * (x - t)**2 * np.exp(-x + t)
+        # u_true = lambda x: 1/3 - 1/3 * np.exp(-3/2 * x) * (
+        # np.cos(np.sqrt(3)/2 * x) + np.sqrt(3) * np.sin(np.sqrt(3)/2 * x))
+        # test = HOHWM.IntegralEquation(linear=True, type="Volterra", f=f, K=K)
 
+        f = lambda x: x
+        K = lambda x, t: - (x - t)
+        u_true = lambda x: np.sin(x)
+        test = HOHWM.IntegralEquation(linear=True, type="Volterra", f=f, K=K)
+        
+        # f = lambda x: 2 * np.cosh(x) - x * np.sinh(x) - 1
+        # K = lambda x, t: t
+        # u_true = lambda x: np.cosh
+        # test = HOHWM.IntegralEquation(linear=True, type="Volterra", f=f, K=K)
+        
+        f = lambda x: x + 1
+        K = lambda x, t: x - t
+        u_true = lambda x: np.exp(x)
+        test = HOHWM.IntegralEquation(linear=True, type="Volterra", f=f, K=K)
+        
     for s in [1, 2]:
         for M in col_size:
-            u_approx_func = test.solve(N = 2*M, s=s, approx_func=True)
-
-            x = np.linspace(0, 1, 101)
+            u_approx_func = test.solve(N = 2*M, s=s, approx_func=True, method="ls")
+            x = HOHWM.collocation(2 * M)
+            # print("x", x)
+            # x = x[1:-1]
             # print("x", x)
             # compute the local error at x = 0.5
-            u_true_half = u_true(0.5)
-            u_haar_approx_half = u_approx_func(0.5)
+            u_true_half = u_true(1/2)
+            u_haar_approx_half = u_approx_func(1/2)
             # store the error
             err_local[col_size.index(M)] = abs(u_true_half - u_haar_approx_half)
             # compute the global error with zero-norm
             u_true_vec = u_true(x)
             u_haar_approx_vec = u_approx_func(x)
-            err_global[col_size.index(M)] = np.linalg.norm(
-                np.abs(u_true_vec - u_haar_approx_vec)) # probably wrong
-        err_global /= 100
+            # err_global[col_size.index(M)] = np.sqrt(np.sum(np.power(np.abs(u_true_vec - u_haar_approx_vec), 2)))
+            err_global[col_size.index(M)] = np.linalg.norm(np.abs(u_true_vec - u_haar_approx_vec)) # probably wrong
+        # err_global /= (2 * M * 100)
         
         # print the results
         print("Linear {} ({}st derivative)".format(test_type, s))
--- HOHWM/IntegralEquation.py
+++ HOHWM/IntegralEquation.py
@@ -1,5 +1,7 @@
 import numpy as np  # noqa
 import matplotlib.pyplot as plt # noqa
+import scipy as sp # noqa
+from scipy import integrate # noqa
 # flake8: noqa
 
 
@@ -73,12 +75,11 @@ def haar_int_2(x, i):
         alpha = k / m
         beta = (k + 0.5) / m
         gamma = (k + 1) / m
-        a = 1. * (x>=alpha) * (x<beta) * (x - alpha) ** 2 / 2
-        b = -1. * (x>=beta) * (x<=gamma) * ((x - gamma) ** 2 / 2 - (beta - alpha) ** 2)
-        c = 1. * (x> gamma) * (x <= 1) * (beta - alpha) ** 2
-        if i != 0 and (i & (i - 1)) == 0: # if i is power of 2
-            c = 0
-        int_2 = a + b + c
+        a = 0 * (x<alpha)
+        b = 1. * (x>=alpha) * (x<beta) * (x - alpha) ** 2 / 2
+        c = 1. * (x>=beta) * (x<gamma) * (-(x - gamma) ** 2 / 2 + 1/(4 * m ** 2))
+        d = 1. * (x>=gamma) * (x<= 1) * (1/(4 * m ** 2))
+        int_2 = a + b + c + d
         return int_2
 
 
@@ -104,7 +105,7 @@ class IntegralEquation:
         self.f = f
         self.K = K
 
-    def solve(self, N, s, plot=False, approx=False, approx_func=False):
+    def solve(self, N, s, plot=False, approx=False, approx_func=False, method='ls'):
         # Make sure N is a power of 2
         N = N
         if N & (N - 1) != 0:
@@ -119,42 +120,95 @@ class IntegralEquation:
 
             if self.type == 'Fredholm':
                 if s == 1:
-                    S_1 = np.zeros(N)
-                    for j in range(N):
+                    if method == 'ls':
+                        S_1 = np.zeros(N)
+                        for j in range(N):
+                            for k in range(N):
+                                S_1[j] += K(0, t[k]) * haar_int_1(t[k], j+1)
+                        S_1 = 1/N * S_1
+
+                        S_2 = 0
                         for k in range(N):
-                            S_1[j] += K(0, t[k]) * haar_int_1(t[k], j+1)
-                    S_1 = 1/N * S_1
+                            S_2 += K(0, t[k])
+                        S_2 = 1/N * S_2
 
-                    S_2 = 0
-                    for k in range(N):
-                        S_2 += K(0, t[k])
-                    S_2 = 1/N * S_2
+                        M_A = np.zeros((N, N))
+                        for j in range(N):
+                            for k in range(N):
+                                M_A[:, j] += K(x, t[k]) * haar_int_1(t[k], j+1)
+                        M_A = haar_int_1_mat(x, N) - 1/N * M_A
 
-                    M_A = np.zeros((N, N))
-                    for j in range(N):
+                        V_B = np.zeros(N)
                         for k in range(N):
-                            M_A[:, j] += K(x, t[k]) * haar_int_1(t[k], j+1)
-                    M_A = haar_int_1_mat(x, N) - 1/N * M_A
+                            V_B += K(x, t[k])
+                        V_B = 1 - 1/N * V_B
 
-                    V_B = np.zeros(N)
-                    for k in range(N):
-                        V_B += K(x, t[k])
-                    V_B = 1 - 1/N * V_B
+                        A_ls = M_A + np.outer(V_B, S_1) / (1 - S_2)
+                        B_ls = f(x) - f(0) * V_B / (1 - S_2)
 
-                    A_ls = M_A + np.outer(V_B, S_1) / (1 - S_2)
-                    B_ls = f(x) - f(0) * V_B / (1 - S_2)
+                        coef_haar = np.linalg.solve(A_ls, B_ls)
 
-                    coef_haar = np.linalg.solve(A_ls, B_ls)
+                        # calculate the approximation
+                        u_haar_approx = np.zeros(N)
+                        for k in range(N):
+                            u_haar_approx += coef_haar[k] * haar_int_1(x, k + 1)
+                        C_1 = 1 / (1 - S_2) * (f(0) + np.dot(coef_haar, S_1))
+                        u_haar_approx += C_1
+                    
+                    if method == 'fsolve':
+                        S_1 = np.zeros(N)
+                        for j in range(N):
+                            for k in range(N):
+                                S_1[j] += K(0, t[k]) * haar_int_1(t[k], j+1)
+                        S_1 = 1/N * S_1
 
-                    # calculate the approximation
-                    u_haar_approx = np.zeros(N)
-                    for k in range(N):
-                        u_haar_approx += coef_haar[k] * haar_int_1(x, k + 1)
-                    C_1 = 1 / (1 - S_2) * (f(0) + np.dot(coef_haar, S_1))
-                    u_haar_approx += C_1
-                    # breakpoint()
+                        S_2 = 0
+                        for k in range(N):
+                            S_2 += K(0, t[k])
+                        S_2 = 1/N * S_2
+
+                        def sys_eqs(vars):
+                            C_1 = vars[0]
+                            coef_haar = vars[1:]
+
+                            N = len(coef_haar)
+                            x = collocation(N)
+                            t = collocation(N)
+
+                            eqs = np.zeros(N + 1)
+                            
+                            # C1 part
+                            eqs[0] = C_1 - 1/(1 - S_2) * (f(0) - np.dot(coef_haar, S_1))
+                            
+                            # Haar part
+                            M_A = np.zeros((N, N))
+                            for j in range(N):
+                                for k in range(N):
+                                    M_A[:, j] += K(x, t[k]) * haar_int_1(t[k], j+1)
+                            M_A = haar_int_1_mat(x, N) - 1/N * M_A
+                            
+                            V_B = np.zeros(N)
+                            for k in range(N):
+                                V_B += K(x, t[k])
+                            V_B = 1 - 1/N * V_B
+                            
+                            eqs[1:] = np.dot(M_A, coef_haar) - (
+                                f(x) - 1/(1 - S_2) * (f(0) + np.dot(coef_haar, S_1)) * V_B
+                            )
+                            return eqs
+
+                        vars = sp.optimize.fsolve(sys_eqs, np.ones(N + 1))
+                        C_1 = vars[0]
+                        coef_haar = vars[1:]
+                        
+                        u_haar_approx = np.zeros(N)
+                        for k in range(N):
+                            u_haar_approx += coef_haar[k] * haar_int_1(x, k + 1)
+                        u_haar_approx += C_1
+                        
                     if plot is True:
                         plt.plot(x, u_haar_approx, label='Approximation')
+                        plt.show()
                     elif approx is True:
                         return u_haar_approx
                     elif approx_func is True:
@@ -164,122 +218,99 @@ class IntegralEquation:
                             for k in range(N):
                                 approx_func_val += coef_haar[k] * haar_int_1(x, k + 1)
                             return approx_func_val
-
-                            # interpolation
-                            # return np.interp(x, collocation(N), u_haar_approx) 
                         return u_haar_approx_func
                     else:
                         return coef_haar
                 elif s == 2:
-                    S_1 = np.zeros(N)
-                    for j in range(N):
+                    if method == 'ls':
+                        S_1 = np.zeros(N)
+                        for j in range(N):
+                            for k in range(N):
+                                S_1[j] += K(0, t[k]) * haar_int_1(t[k], j+1)
+                        S_1 = 1/N * S_1
+
+                        S_2 = 0
                         for k in range(N):
-                            S_1[j] += K(0, t[k]) * haar_int_1(t[k], j+1)
-                    S_1 = 1/N * S_1
-
-                    S_2 = 0
-                    for k in range(N):
-                        S_2 += K(0, t[k])
-                    S_2 = 1/N * S_2
+                            S_2 += K(0, t[k])
+                        S_2 = 1/N * S_2
 
 
-                    S_3 = 0
-                    for k in range(N):
-                        S_3 += K(1, t[k])
-                    S_3 = 1/N * S_3
-
-                    S_4 = 0
-                    for k in range(N):
-                        S_4 += K(0, t[k]) * t[k]
-                    S_4 = 1/N * S_4
-
-                    S_5 = 0
-                    for k in range(N):
-                        S_5 += K(1, t[k]) * t[k]
-                    S_5 = 1/N * S_5
-
-                    S_6 = np.zeros(N)
-                    for j in range(N):
+                        S_3 = 0
                         for k in range(N):
-                            S_6[j] += K(0, t[k]) * haar_int_2(t[k], j+1)
-                    S_6 = 1/N * S_6
+                            S_3 += K(1, t[k])
+                        S_3 = 1/N * S_3
 
-                    S_7 = np.zeros(N)
-                    for j in range(N):
+                        S_4 = 0
                         for k in range(N):
-                            S_7[j] += K(1, t[k]) * haar_int_2(t[k], j+1)
-                    S_7 = 1/N * S_7
+                            S_4 += K(0, t[k]) * t[k]
+                        S_4 = 1/N * S_4
 
-                    S_8 = 1 - S_2 + S_4 * (1 - S_3) - S_5 * (1 - S_2)
-                    
-                    A = f(0) * (1 - S_5) + f(1) * S_4
-
-                    D = -f(0) * (1 - S_3) + f(1) * (1 - S_2)
-
-                    V_B = np.zeros(N)
-                    for i in range(N):
-                        V_B[i] = haar_int_2(1, i + 1)
-                    V_B -= S_7
-
-                    V_E = (1 - S_5) * S_6 - S_4 * V_B
-
-                    V_F = (1 - S_3) * S_6 + (1 - S_2) * V_B
-                    
-                    M_A = np.zeros((N, N))
-                    for i in range(N):
+                        S_5 = 0
                         for k in range(N):
-                            M_A[:, i] += K(x, t[k]) * haar_int_2(t[k], i + 1)
-                    M_A = haar_int_2_mat(x, N) - 1/N * M_A
-                    
-                    V_P = np.zeros(N)
-                    for k in range(N):
-                        V_P += K(x, t[k])
-                    V_P = 1 - 1/N * V_P
+                            S_5 += K(1, t[k]) * t[k]
+                        S_5 = 1/N * S_5
 
-                    V_Q = np.zeros(N)
-                    for k in range(N):
-                        V_Q += K(x, t[k]) * t[k]
-                    V_Q = x - 1/N * V_Q
-                    
-                    LHS_ls = M_A + np.outer(V_P, V_E) / S_8 - np.outer(V_Q, V_F) / S_8 # if bug, check here
-                    RHS_ls = f(x) - A * V_P / S_8 - D * V_Q / S_8
-                    
-                    coef_haar = np.linalg.solve(LHS_ls, RHS_ls)
-                    
-                    u_haar_approx = np.zeros(N)
-                    for k in range(N):
-                        u_haar_approx += coef_haar[k] * haar_int_2(x, k + 1)
-                    C1 = 1 / S_8 * (A + np.dot(coef_haar, V_E))
-                    C2 = 1 / S_8 * (D - np.dot(coef_haar, V_F))
-                    u_haar_approx += C1 + C2 * x
-                    
-                    # print the coefficients and constants
-                    # print("C1 = ", C1)
-                    # print("C2 = ", C2)
-                    # print("coef_haar = ", coef_haar)
-                    # print("x = ", x)
-                    # print("t = ", t)                    
-                    # print("S2 = ", S_2)
-                    # print("S3 = ", S_3)
-                    # print("S4 = ", S_4)
-                    # print("S5 = ", S_5)
-                    # print("S6 = ", S_6)
-                    # print("S7 = ", S_7)
-                    # print("S8 = ", S_8)
-                    # print("A = ", A)
-                    # print("D = ", D)
-                    # print("V_B = ", V_B)
-                    # print("V_E = ", V_E)
-                    # print("V_F = ", V_F)
-                    # print("M_A = ", M_A)
-                    # print("V_P = ", V_P)
-                    # print("V_Q = ", V_Q)
-                    # print("LHS_ls = ", LHS_ls)
-                    # print("RHS_ls = ", RHS_ls)
+                        S_6 = np.zeros(N)
+                        for j in range(N):
+                            for k in range(N):
+                                S_6[j] += K(0, t[k]) * haar_int_2(t[k], j+1)
+                        S_6 = 1/N * S_6
 
+                        S_7 = np.zeros(N)
+                        for j in range(N):
+                            for k in range(N):
+                                S_7[j] += K(1, t[k]) * haar_int_2(t[k], j+1)
+                        S_7 = 1/N * S_7
+
+                        S_8 = 1 - S_2 + S_4 * (1 - S_3) - S_5 * (1 - S_2)
+                        
+                        A = f(0) * (1 - S_5) + f(1) * S_4
+
+                        D = -f(0) * (1 - S_3) + f(1) * (1 - S_2)
+
+                        V_B = np.zeros(N)
+                        for i in range(N):
+                            V_B[i] = haar_int_2(1, i + 1)
+                        V_B -= S_7
+
+                        V_E = (1 - S_5) * S_6 - S_4 * V_B
+
+                        V_F = (1 - S_3) * S_6 + (1 - S_2) * V_B
+                        
+                        M_A = np.zeros((N, N))
+                        for i in range(N):
+                            for k in range(N):
+                                M_A[:, i] += K(x, t[k]) * haar_int_2(t[k], i + 1)
+                        M_A = haar_int_2_mat(x, N) - 1/N * M_A
+                        
+                        V_P = np.zeros(N)
+                        for k in range(N):
+                            V_P += K(x, t[k])
+                        V_P = 1 - 1/N * V_P
+
+                        V_Q = np.zeros(N)
+                        for k in range(N):
+                            V_Q += K(x, t[k]) * t[k]
+                        V_Q = x - 1/N * V_Q
+                        
+                        LHS_ls = M_A + np.outer(V_P, V_E) / S_8 - np.outer(V_Q, V_F) / S_8 # if bug, check here
+                        RHS_ls = f(x) - A * V_P / S_8 - D * V_Q / S_8
+                        
+                        coef_haar = np.linalg.solve(LHS_ls, RHS_ls)
+                        
+                        u_haar_approx = np.zeros(N)
+                        for k in range(N):
+                            u_haar_approx += coef_haar[k] * haar_int_2(x, k + 1)
+                        C1 = 1 / S_8 * (A + np.dot(coef_haar, V_E))
+                        C2 = 1 / S_8 * (D - np.dot(coef_haar, V_F))
+                        u_haar_approx += C1 + C2 * x
+                    
+                    if method == 'fsolve':
+                        raise NotImplementedError()
                     
                     if plot is True:
                         plt.plot(x, u_haar_approx, label='Approximation')
+                        plt.show()
                     elif approx is True:
                         return u_haar_approx
                     elif approx_func is True:
@@ -430,9 +461,13 @@ class IntegralEquation:
 
 # # Fredholm
 # N = 64
-# f = lambda x: np.exp(x) + np.exp(-x)
-# K = lambda x, t: -np.exp(-(x + t))
-# u_true = lambda x: np.exp(x)
+# # f = lambda x: np.exp(x) + np.exp(-x)
+# # K = lambda x, t: -np.exp(-(x + t))
+# # u_true = lambda x: np.exp(x)
+# # test = IntegralEquation(linear=True, type="Fredholm", f=f, K=K)
+# f = lambda x: 5/6 * x - 1/9
+# K = lambda x, t: 1/3 * (x + t)
+# u_true = lambda x: x
 # test = IntegralEquation(linear=True, type="Fredholm", f=f, K=K)
 
 
@@ -450,37 +485,6 @@ class IntegralEquation:
 # u_haar_approx_half = u_approx_func(0.5)
 # print(abs(u_true_half - u_haar_approx_half))
 
-# # Volterra
-# N = 64
-# f = lambda x: 1/2 * x**2 * np.exp(-x)
-# K = lambda x, t: 1/2 * (x - t)**2 * np.exp(-x + t)
-# u_true = lambda x: 1/3 - 1/3 * np.exp(-3/2 * x) * (
-#     np.cos(np.sqrt(3)/2 * x) + np.sqrt(3) * np.sin(np.sqrt(3)/2 * x))
-# test = IntegralEquation(linear=True, type="Volterra", f=f, K=K)
-
-# u_approx_func = test.solve(N = N, s=2, approx_func=True)
-# x = np.linspace(0, 1, 101)
-# # another figure
-# plt.figure()
-# plt.plot(x, u_true(x), label='True solution')
-# plt.plot(x, u_approx_func(x), label='Approximation', linestyle=':')
-# plt.legend()
-# plt.plot(0.5, u_true(0.5), 'o', color='black', markersize=1)
-# plt.plot(0.5, u_approx_func(0.5), 'o', color='Green', markersize=1)
-# plt.savefig('Linspace_Volterra.png', dpi=300)
-# u_true_half = u_true(0.5)
-# u_haar_approx_half = u_approx_func(0.5)
-# print(abs(u_true_half - u_haar_approx_half))
-
-
-# # Fredholm
-# N = 64
-# f = lambda x: np.exp(x) + np.exp(-x)
-# K = lambda x, t: -np.exp(-(x + t))
-# u_true = lambda x: np.exp(x)
-# test = IntegralEquation(linear=True, type="Fredholm", f=f, K=K)
-
-
 # u_approx_func = test.solve(N = N, s=2, approx_func=True)
 # x = collocation(N)
 # plt.figure()
@@ -495,15 +499,35 @@ class IntegralEquation:
 # u_haar_approx_half = u_approx_func(0.5)
 # print(abs(u_true_half - u_haar_approx_half))
 
-# # Volterra
+# Volterra
 # N = 64
-# f = lambda x: 1/2 * x**2 * np.exp(-x)
-# K = lambda x, t: 1/2 * (x - t)**2 * np.exp(-x + t)
-# u_true = lambda x: 1/3 - 1/3 * np.exp(-3/2 * x) * (
-#     np.cos(np.sqrt(3)/2 * x) + np.sqrt(3) * np.sin(np.sqrt(3)/2 * x))
+# # f = lambda x: 1/2 * x**2 * np.exp(-x)
+# # K = lambda x, t: 1/2 * (x - t)**2 * np.exp(-x + t)
+# # u_true = lambda x: 1/3 - 1/3 * np.exp(-3/2 * x) * (
+# #     np.cos(np.sqrt(3)/2 * x) + np.sqrt(3) * np.sin(np.sqrt(3)/2 * x))
+# # test = IntegralEquation(linear=True, type="Volterra", f=f, K=K)
+
+# f = lambda x: x + 1
+# K = lambda x, t: x - t
+# u_true = lambda x: np.exp(x)
 # test = IntegralEquation(linear=True, type="Volterra", f=f, K=K)
 
-# u_approx_func = test.solve(N = N, s=2, approx_func=True)
+# u_approx_func = test.solve(N = N, s=1, approx_func=True)
+# x = np.linspace(0, 1, 101)
+# # another figure
+# plt.figure()
+# plt.plot(x, u_true(x), label='True solution')
+# plt.plot(x, u_approx_func(x), label='Approximation', linestyle=':')
+# plt.legend()
+# plt.plot(0.5, u_true(0.5), 'o', color='black', markersize=1)
+# plt.plot(0.5, u_approx_func(0.5), 'o', color='Green', markersize=1)
+# plt.savefig('Linspace_Volterra.png', dpi=300)
+# u_true_half = u_true(0.5)
+# u_haar_approx_half = u_approx_func(0.5)
+# print(abs(u_true_half - u_haar_approx_half))
+
+
+# u_approx_func = test.solve(N = N, s=1, approx_func=True)
 # x = collocation(N)
 # # another figure
 # plt.figure()
